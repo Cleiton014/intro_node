4ae815b00ec5833f6fc8886731c8a3dca97ca1d6
Cleiton014 <159494633+Cleiton014@users.noreply.github.com>
2024-02-15 20:41:26 -0300

    aula 15-02-24

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("Olá mundo!");
+let nome = "cleiton";
+console.log("Olá " + nome);
+let idade = 23;
+let altura = 1.75;
+console.log("Nome: " + nome + " - sua idade é: " + idade + " - e sua altura é: " +altura);
+
+let trabalha = true;
+
+console.log("trabalha: " + (trabalha === true ? "Sim" : "Não"));
